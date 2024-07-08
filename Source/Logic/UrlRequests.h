c2145e473c357171e83d46ddcac72357d2c2b55a
Jake Adamson <Jake-The-Human@users.noreply.github.com>
2024-07-08 17:07:41 -0400

    Update UrlRequests.h
    filled in functions

--- Source/Logic/UrlRequests.h
+++ Source/Logic/UrlRequests.h
@@ -2,6 +2,7 @@
 
 #include <JuceHeader.h>
 #include "Song.h"
+#include <functional>
 #include <vector>
 
 // for more info check this out B^)
@@ -31,12 +32,69 @@ class URLFetcher : public juce::ThreadPoolJob
 {
   public:
     virtual ~URLFetcher() = default;
-    URLFetcher() : juce::ThreadPoolJob("URLFetcher") {}
+    URLFetcher(const juce::String& request, const juce::StringPairArray& queryParams, std::function<void(const juce::String&)> callback) : 
+    juce::ThreadPoolJob("URLFetcher"),
+    requestType(request),
+    queryParamsInternal(queryParams),
+    callbackInternal(callback) 
+    {
+        queryParamsInternal.set("u", "admin");
+        queryParamsInternal.set("p", "admin");
+        queryParamsInternal.set("c", "Waxy"); // A unique string identifying the client application.
+        queryParamsInternal.set("f", "json");
+    }
+
     juce::ThreadPoolJob::JobStatus runJob() override
     {
-      return {};
+        juce::URL urlObj("http://localhost:4747/rest/" + requestType);
+        urlObj = urlObj.withParameters(queryParamsInternal);
+
+        juce::String headers = "Content-Type: application/json";
+        int timeOutMs = 30000; // Timeout in milliseconds
+        juce::StringPairArray responseHeaders;
+        int statusCode = 0;
+        int numRedirectsToFollow = 5; // Number of redirects to follow
+
+        juce::URL::InputStreamOptions options(
+            juce::URL::ParameterHandling::inAddress);
+        
+        std::unique_ptr<juce::InputStream> stream = urlObj.createInputStream(options);
+        juce::String response;
+
+        if (stream != nullptr)
+        {
+          response = stream->readEntireStreamAsString();
+          DBG("Response: " + response);
+          DBG("Status code: " + juce::String(statusCode));
+
+          // Print response headers
+          for (int i = 0; i < responseHeaders.size(); ++i)
+          {
+            DBG(responseHeaders.getAllKeys()[i] + ": " +
+                responseHeaders.getAllValues()[i]);
+          }
+        }
+        else
+        {
+          DBG("Failed to create input stream!");
+        }
+        
+        if (response.isEmpty())
+        {
+            juce::Logger::writeToLog("Failed to fetch URL: " + requestType);
+        }
+        else
+        {
+            callbackInternal(response);
+        }
+
+        return juce::ThreadPoolJob::JobStatus::jobHasFinished;   
     }
+
   private:
-   juce::String url;
-   std::function<void(const juce::String&)> callback;
+   juce::String requestType;
+   juce::StringPairArray queryParamsInternal;
+   std::function<void(const juce::String&)> callbackInternal;
+
+   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (URLFetcher)
 };
