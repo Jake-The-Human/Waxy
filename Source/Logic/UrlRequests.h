72ac06e35c77e8be44ff39d92fcf9f02212f3690
Jake Adamson <Jake-The-Human@users.noreply.github.com>
2024-07-08 17:35:05 -0400

    Update UrlRequests.h
    ran formatter

--- Source/Logic/UrlRequests.h
+++ Source/Logic/UrlRequests.h
@@ -30,78 +30,76 @@ namespace UrlRequests
 // seprate the responce and request. bellow will be the class that makes the responce
 class URLFetcher : public juce::ThreadPoolJob
 {
-  public:
-    virtual ~URLFetcher() = default;
-    URLFetcher(const juce::String& request, const juce::StringPairArray& queryParams, std::function<void(const juce::String&)> callback) : 
-    juce::ThreadPoolJob("URLFetcher"),
-    requestType(request),
-    queryParamsInternal(queryParams),
-    callbackInternal(callback) 
+public:
+  virtual ~URLFetcher() = default;
+  URLFetcher(const juce::String &request, const juce::StringPairArray &queryParams, std::function<void(const juce::String &)> callback) : juce::ThreadPoolJob("URLFetcher"),
+                                                                                                                                          requestType(request),
+                                                                                                                                          queryParamsInternal(queryParams),
+                                                                                                                                          callbackInternal(callback)
+  {
+    queryParamsInternal.set("u", "admin");
+    queryParamsInternal.set("p", "admin");
+    queryParamsInternal.set("c", "Waxy"); // A unique string identifying the client application.
+    queryParamsInternal.set("f", "json");
+  }
+
+  juce::ThreadPoolJob::JobStatus runJob() override
+  {
+    juce::URL urlObj("http://localhost:4747/rest/" + requestType);
+    urlObj = urlObj.withParameters(queryParamsInternal);
+
+    // Not sure if we need this so i put here before i start remove prev impl
+    // Add POST data if provided
+    // if (!postData.isEmpty())
+    // {
+    //   url = url.withPOSTData(postData);
+    // }
+
+    juce::String headers = "Content-Type: application/json";
+    int timeOutMs = 30000; // Timeout in milliseconds
+    juce::StringPairArray responseHeaders;
+    int statusCode = 0;
+    int numRedirectsToFollow = 5; // Number of redirects to follow
+
+    juce::URL::InputStreamOptions options(juce::URL::ParameterHandling::inAddress);
+
+    std::unique_ptr<juce::InputStream> stream = urlObj.createInputStream(options);
+    juce::String response;
+
+    if (stream != nullptr)
     {
-        queryParamsInternal.set("u", "admin");
-        queryParamsInternal.set("p", "admin");
-        queryParamsInternal.set("c", "Waxy"); // A unique string identifying the client application.
-        queryParamsInternal.set("f", "json");
+      response = stream->readEntireStreamAsString();
+      DBG("Response: " + response);
+      DBG("Status code: " + juce::String(statusCode));
+
+      // Print response headers
+      for (int i = 0; i < responseHeaders.size(); ++i)
+      {
+        DBG(responseHeaders.getAllKeys()[i] + ": " +
+            responseHeaders.getAllValues()[i]);
+      }
+    }
+    else
+    {
+      DBG("Failed to create input stream!");
     }
 
-    juce::ThreadPoolJob::JobStatus runJob() override
+    if (response.isEmpty())
     {
-        juce::URL urlObj("http://localhost:4747/rest/" + requestType);
-        urlObj = urlObj.withParameters(queryParamsInternal);
-
-        // Not sure if we need this so i put here before i start remove prev impl
-        // Add POST data if provided
-        // if (!postData.isEmpty())
-        // {
-        //   url = url.withPOSTData(postData);
-        // } 
-
-        juce::String headers = "Content-Type: application/json";
-        int timeOutMs = 30000; // Timeout in milliseconds
-        juce::StringPairArray responseHeaders;
-        int statusCode = 0;
-        int numRedirectsToFollow = 5; // Number of redirects to follow
-
-        juce::URL::InputStreamOptions options(
-            juce::URL::ParameterHandling::inAddress);
-        
-        std::unique_ptr<juce::InputStream> stream = urlObj.createInputStream(options);
-        juce::String response;
-
-        if (stream != nullptr)
-        {
-          response = stream->readEntireStreamAsString();
-          DBG("Response: " + response);
-          DBG("Status code: " + juce::String(statusCode));
-
-          // Print response headers
-          for (int i = 0; i < responseHeaders.size(); ++i)
-          {
-            DBG(responseHeaders.getAllKeys()[i] + ": " +
-                responseHeaders.getAllValues()[i]);
-          }
-        }
-        else
-        {
-          DBG("Failed to create input stream!");
-        }
-        
-        if (response.isEmpty())
-        {
-            juce::Logger::writeToLog("Failed to fetch URL: " + requestType);
-        }
-        else
-        {
-            callbackInternal(response);
-        }
-
-        return juce::ThreadPoolJob::JobStatus::jobHasFinished;   
+      juce::Logger::writeToLog("Failed to fetch URL: " + requestType);
+    }
+    else
+    {
+      callbackInternal(response);
     }
 
-  private:
-   juce::String requestType;
-   juce::StringPairArray queryParamsInternal;
-   std::function<void(const juce::String&)> callbackInternal;
+    return juce::ThreadPoolJob::JobStatus::jobHasFinished;
+  }
 
-   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (URLFetcher)
+private:
+  juce::String requestType;
+  juce::StringPairArray queryParamsInternal;
+  std::function<void(const juce::String &)> callbackInternal;
+
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(URLFetcher)
 };
